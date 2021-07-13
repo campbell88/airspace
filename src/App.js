215e651ba69388034b0b8c95f336fb7a4bf7aa46
campbell88 <shatharcampbell@gmail.com>
2021-07-13 10:26:21 -0400

    added polygon

--- src/App.js
+++ src/App.js
@@ -1,50 +1,42 @@
 import React, {Component} from "react";
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
-
-const mapStyles = {
-  width: '100%',
-  height: '100%',
-};
-
+import { Map, GoogleApiWrapper, Marker, Polygon } from 'google-maps-react';
 
 export class MapContainer extends Component {
+
   constructor(props) {
- 
     super(props);
 
-    this.state = {
-      stores: [,
-              {latitude: 42.23982914405, longitude: -83.35447311401367},
-              {latitude: 42.20995654939329, longitude: -83.38743209838867},
-              {latitude: 42.18452193135192, longitude: -83.37146759033203},
-              {latitude: 42.18719304724962, longitude: -83.3536148071289},
-              {latitude: 42.19380675326742, longitude: -83.32340240478514},
-              {latitude: 42.23118661249613, longitude: -83.31430435180664},
-              {latitude: 42.24160834186122, longitude: -83.34932327270508},
-              {latitude: 42.23982914405, longitude: -83.35447311401367},
-            ]
-    }
-  }
-  displayMarkers = () => {
-    return this.state.stores.map((store, index) => {
-      return <Marker key={index} id={index} position={{
-       lat: store.latitude,
-       lng: store.longitude
-     }}
-     onClick={() => console.log("You clicked me!")} />
-    })
   }
 
   render() {
+    const polygonCoords = [
+                {lat: 42.23982914405, lng: -83.35447311401367},
+                {lat: 42.20995654939329, lng: -83.38743209838867},
+                {lat: 42.18452193135192, lng: -83.37146759033203},
+                {lat: 42.18719304724962, lng: -83.3536148071289},
+                {lat: 42.19380675326742, lng: -83.32340240478514},
+                {lat: 42.23118661249613, lng: -83.31430435180664},
+                {lat: 42.24160834186122, lng: -83.34932327270508},
+                {lat: 42.23982914405, lng: -83.35447311401367},
+              ]
+
     return (
+      <div className="map-container">
         <Map
           google={this.props.google}
+          className={"map"}
           zoom={11}
-          style={mapStyles}
-          initialCenter={{ lat: 42.331429, lng: -83.045753}}
+          initialCenter={{ lat: 42.220520, lng: -83.385307}}
         >
-          {this.displayMarkers()}
+          <Polygon
+            paths={polygonCoords}
+            strokeOpacity={0.8}
+            strokeWeight={2}
+            fillColor={"D3D3D3"}
+            fillOpacity={0.35}
+          />
         </Map>
+      </div>
     );
   }
 }
